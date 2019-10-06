a53b231cabd710949bf7121dc5284021fba5db2f
unknown <vitbolb1@gmail.com>
2019-10-05 23:40:28 -0300

    Add User model and SessionController

---
+++ src/controllers/SessionController.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import { Request, Response } from 'express';
+import User from '../models/User';
+
+export default {
+    async store(req: Request, res: Response) {
+        const { email } = req.body;
+
+        let user = await User.findOne({ email });
+
+        if (!user) {
+            user = await User.create({ email });
+        }
+
+        return res.json(user);
+    },
+};
